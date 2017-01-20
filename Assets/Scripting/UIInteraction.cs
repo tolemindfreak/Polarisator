14c99b42e0af8dcbc20c5b8cc7720835c3444bd4
DESKTOP-RTJ02CG\Andhika Rizky <andhikarizkysetiawan@gmail.com>
2017-01-21 01:57:06 +0700

    Nambahain UI dan Gambar

---
+++ Assets/Scripting/UIInteraction.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class UIInteraction : MonoBehaviour {
+	public static UIInteraction instance;
+
+	[Header("MainMenu UI")]
+	public CanvasGroup m_MainMenuPanel;
+
+	[Header("Level UI")]
+	public CanvasGroup m_LevelPanel;
+
+	[Header("Quit UI")]
+	public CanvasGroup m_QuitPanel;
+
+	public void OpenLevel()
+	{
+		m_LevelPanel.alpha = 0;
+		m_LevelPanel.gameObject.SetActive (true);
+
+		m_LevelPanel.DOFade(1, 0.5f).OnComplete(() =>
+			{
+
+			});
+	}
+}
